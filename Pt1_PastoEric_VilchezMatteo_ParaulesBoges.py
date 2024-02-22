81f59c17311a001c1cac00e944e6b1e0e367c545
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-02-22 08:50:54 +0100

    Update Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py

--- Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
+++ Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
@@ -1,4 +1,4 @@
-# Pt1_PastoEric_VilchezMatteo_ASIXc1C_ParaulesBoges"""
+"""
     Eric Pasto i Matteo Vilchez.
     22/02/2024
     ASIXc 1B  M03 UF2 A1
@@ -6,5 +6,9 @@
     Fer un programa que demani una frase per teclat i la retorni per pantalla
     amb les lletres de cada paraula de la frase desordenada
 """
-#Pedir la frase
+#1 Pedir un texto
+#2 Separamos cada palabra de la frase para que se desordene
+#3 Desordenar la frase teniendo en cuenta que la primera y ultima letra no se mueven
+#4 Devolvemos todo el texto desordenado
+#5 Escribimos un "quiere decir" y ahí mostrará el texto original que mandó el usuario
 frase =input("Dime una frase: ")
