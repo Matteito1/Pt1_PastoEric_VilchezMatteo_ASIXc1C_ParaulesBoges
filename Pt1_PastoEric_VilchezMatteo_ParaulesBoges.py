8cb1034a321d84b8c4e62269f7e209bc943d5a09
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-02-22 09:50:51 +0100

    Update Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py

--- Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
+++ Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
@@ -7,8 +7,25 @@
     amb les lletres de cada paraula de la frase desordenada
 """
 #1 Pedir un texto
+#1.2 Tener en cuenta los caracteres especiales
 #2 Separamos cada palabra de la frase para que se desordene
 #3 Desordenar la frase teniendo en cuenta que la primera y ultima letra no se mueven
-#4 Devolvemos todo el texto desordenado
+#4 Devolvemos toddo el texto desordenado
 #5 Escribimos un "quiere decir" y ahí mostrará el texto original que mandó el usuario
-frase =input("Dime una frase: ")
+
+texto_ord = []
+cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
+def frase():
+    texto = input("Dime el texto a desordenar: ")
+    texto_ord=texto.split
+    return texto,texto_ord
+def desordenar_palabras():
+    for palabrades in texto_ord:
+     pLetra = palabrades[0]
+     uLetra = palabrades[-1]
+     print(pLetra)
+     print(uLetra)
+
+frase()
+desordenar_palabras()
+print(texto_ord)
