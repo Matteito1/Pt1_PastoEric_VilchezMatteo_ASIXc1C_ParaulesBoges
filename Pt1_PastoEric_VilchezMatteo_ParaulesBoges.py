794ed9c0cdcc28e82139fac69102fa81b4e6a0f1
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-02-22 08:45:55 +0100

    Create Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py

---
+++ Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
@@ -0,0 +1,10 @@
+# Pt1_PastoEric_VilchezMatteo_ASIXc1C_ParaulesBoges"""
+    Eric Pasto i Matteo Vilchez.
+    22/02/2024
+    ASIXc 1B  M03 UF2 A1
+    ParaulesBoges Pt.1
+    Fer un programa que demani una frase per teclat i la retorni per pantalla
+    amb les lletres de cada paraula de la frase desordenada
+"""
+#Pedir la frase
+frase =input("Dime una frase: ")
