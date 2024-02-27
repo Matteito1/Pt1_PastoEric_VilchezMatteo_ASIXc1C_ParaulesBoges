a770373907b49f9d8e999dce00a79ac5ceb2d051
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-02-27 10:37:49 +0100

    Update Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py

--- Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
+++ Pt1_PastoEric_VilchezMatteo_ParaulesBoges.py
@@ -13,43 +13,43 @@
 #4 Devolvemos toddo el texto desordenado
 #5 Escribimos un "quiere decir" y ahí mostrará el texto original que mandó el usuario
 
-texto_ord = []
+
+import random
 cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
-def frase():
-    texto = input("Dime el texto a desordenar: ")
-    texto_ord=texto.split
-    return texto,texto_ord
-def desordenar_palabras():
-    for palabrades in texto_ord:
-     pLetra = palabrades[0]
-     uLetra = palabrades[-1]
-     print(pLetra)
-     print(uLetra)
-
-frase()
-desordenar_palabras()
-print(texto_ord)
-
-
-
-
-
-!!!!!!!!!!!!!!
-
-
-listapalabras = []
-cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
-def frase():
-    texto = input("Dime el texto a desordenar: ").split(" ")
-    listapalabras.append(texto)
-    return texto
-def desordenar_palabras():
-    for palabrades in listapalabras:
-     pLetra = palabrades[0]
-     uLetra = palabrades[-1]
-     print(pLetra)
-     print(uLetra)
-
-frase()
-desordenar_palabras()
-print(listapalabras)
+letras=('a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z')
+Texto_Ordenado=[]
+palabras=[]
+frasedes=[]
+def int_frase():
+    try:
+        frase=input("Introduce el texto que quieres desordenar: ")
+        frase_ordenada=frase.split()
+        return frase_ordenada,frase
+    except:
+        print("Error incorrecto lo que has introducido")
+def separar():
+    for palabrades in Texto_Ordenado:
+        if len(palabrades)>=3:
+            if palabrades[-1] in letras:
+                sep=list(palabrades[1:-1])
+                random.shuffle(sep)
+                separado=palabrades[0]+''.join(sep)+palabrades[-1]
+            else:
+                sep=list(palabrades[1:-2])
+                random.shuffle(sep)
+                separado=palabrades[0]+''.join(sep)+palabrades[-2]+palabrades[-1]
+        else:
+            separado=palabrades
+        palabras.append(separado)
+    return palabras
+def juntar():
+    frasedes=' '.join(palabras)
+    return frasedes
+def resultado():
+    print(frasedes)
+    print("Traducido:")
+    print(frase)
+Texto_Ordenado,frase=int_frase()
+palabras=separar()
+frasedes=juntar()
+resultado()
